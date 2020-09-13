ad4f3142e53c47ade9a231d6def210cc45b5e5de
msaviola <67287151+msaviola@users.noreply.github.com>
2020-09-12 17:08:58 -0700

    classes

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+var fs = require ('fa');
 const inquirer = require('inquirer');
 
 const Intern = require("./lib/Intern");
+
+const InternQuestion = () => {
+    inquirer
+      .prompt([{
+        type: 'confirm',
+        name: 'interns',
+        message: "Do you have any interns (y/N)?"
+      }])
+  
+
+
+  .then(function (textOutput) {
+    return thenableWriteFile('./output/TeamProfile.html', htmlOutput);
+})
+.then(function () {
+    console.log('All done!');
+})
+.catch(function (error) {
+    console.log('Oh noes! An error!', error);
+});
+}
+  
